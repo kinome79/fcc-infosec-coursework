7584b7ba2b8cd24f073acb59220f85d20068327c
Todd Alloway <kinome79@yahoo.com>
2023-02-07 17:38:13 -0500

    Finished

--- myApp.js
+++ myApp.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+//Protect the page with helmet options
+app.use(helmet.hidePoweredBy());
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
+//Set secure access requirement for 90 days 
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}))
 
-
-
+//Additional Helmet protections
+app.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"]}
+  }, 
+  dnsPrefetchControl: false,
+  noCache: true,
+}));
 
 
 
